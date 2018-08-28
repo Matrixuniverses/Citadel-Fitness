128e4fb92c3d7caa478140aca7970ea61de38534
Luke Walsh <lwa383@uclive.ac.nz>
2018-08-28 15:11:39 +1200

    updates fxml layouts

---
+++ team2fitness/src/main/java/seng202/group2/view/Controller.java
@@ -0,0 +1,27 @@
+package seng202.group2.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    private AnchorPane mapViewScene;
+    @FXML
+    private AnchorPane addDataScene;
+    @FXML
+    private AnchorPane targetScene;
+    @FXML
+    private AnchorPane manageDataScene;
+    @FXML
+    private AnchorPane viewGraphScene;
+    @FXML
+    private AnchorPane myProfileScene;
+    @FXML
+    private AnchorPane exitScene;
+
+
+    public void insertInfoPane() {
+
+    }
+}
