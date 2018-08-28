126c47ed1ee3b4a89045220635d635f2680e7e02
Adam Conway <aco108@uclive.ac.nz>
2018-08-28 15:15:57 +1200

    Updated controller getter and setter

--- team2fitness/src/main/java/seng202/group2/view/Controller.java
+++ team2fitness/src/main/java/seng202/group2/view/Controller.java
@@ -5,6 +5,73 @@ import javafx.scene.layout.AnchorPane;
 
 public class Controller {
 
+
+    public AnchorPane getMainContainer() {
+        return mainContainer;
+    }
+
+    public void setMainContainer(AnchorPane mainContainer) {
+        this.mainContainer = mainContainer;
+    }
+
+    public AnchorPane getMapViewScene() {
+        return mapViewScene;
+    }
+
+    public void setMapViewScene(AnchorPane mapViewScene) {
+        this.mapViewScene = mapViewScene;
+    }
+
+    public AnchorPane getAddDataScene() {
+        return addDataScene;
+    }
+
+    public void setAddDataScene(AnchorPane addDataScene) {
+        this.addDataScene = addDataScene;
+    }
+
+    public AnchorPane getTargetScene() {
+        return targetScene;
+    }
+
+    public void setTargetScene(AnchorPane targetScene) {
+        this.targetScene = targetScene;
+    }
+
+    public AnchorPane getManageDataScene() {
+        return manageDataScene;
+    }
+
+    public void setManageDataScene(AnchorPane manageDataScene) {
+        this.manageDataScene = manageDataScene;
+    }
+
+    public AnchorPane getViewGraphScene() {
+        return viewGraphScene;
+    }
+
+    public void setViewGraphScene(AnchorPane viewGraphScene) {
+        this.viewGraphScene = viewGraphScene;
+    }
+
+    public AnchorPane getMyProfileScene() {
+        return myProfileScene;
+    }
+
+    public void setMyProfileScene(AnchorPane myProfileScene) {
+        this.myProfileScene = myProfileScene;
+    }
+
+    public AnchorPane getExitScene() {
+        return exitScene;
+    }
+
+    public void setExitScene(AnchorPane exitScene) {
+        this.exitScene = exitScene;
+    }
+
+    @FXML
+    private AnchorPane mainContainer;
     @FXML
     private AnchorPane mapViewScene;
     @FXML
@@ -24,4 +91,6 @@ public class Controller {
     public void insertInfoPane() {
 
     }
+
+
 }
