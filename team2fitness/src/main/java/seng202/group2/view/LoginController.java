cc0ab7f3734472168d75dbd6aae4632df9bdda2a
Luke Walsh <lwa383@uclive.ac.nz>
2018-09-12 14:27:30 +1200

    started implementing login pushing to finish work at home, lab computer too slow

--- team2fitness/src/main/java/seng202/group2/view/LoginController.java
+++ team2fitness/src/main/java/seng202/group2/view/LoginController.java
@@ -1,4 +1,28 @@
 package seng202.group2.view;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+
+    @FXML
+    Button newUserButton;
+
+
+
+    public Button getNewUserButton() {
+        return newUserButton;
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
