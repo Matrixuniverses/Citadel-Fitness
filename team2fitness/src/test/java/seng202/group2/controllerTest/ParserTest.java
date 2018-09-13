e6905111b32d740667cf6caa9335107c4e58818b
osh17 <osh17@uclive.ac.nz>
2018-09-13 13:16:08 +1200

    Added Parser tests

--- team2fitness/src/test/java/seng202/group2/controllerTest/ParserTest.java
+++ team2fitness/src/test/java/seng202/group2/controllerTest/ParserTest.java
@@ -22,7 +22,7 @@ public class ParserTest {
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals("Run around the block", test.get(0).getActivityName());
 
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
@@ -35,7 +35,7 @@ public class ParserTest {
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals("Walk in the woods", test.get(0).getActivityName());
 
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
@@ -47,7 +47,7 @@ public class ParserTest {
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals("Walk with dog", test.get(4).getActivityName());
 
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
@@ -59,7 +59,7 @@ public class ParserTest {
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals("Hike in the mountains", test.get(5).getActivityName());
 
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
@@ -78,7 +78,7 @@ public class ParserTest {
             distance += DataAnalyzer.calcDistance(30.248482, -97.820708, 30.24915, -97.820722);
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals(distance, test.get(0).getTotalDistance());
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
@@ -95,45 +95,7 @@ public class ParserTest {
             distance += DataAnalyzer.calcDistance(30.24925263, -97.82029614, 30.24929379, -97.82031097);
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals(distance, test.get(6).getTotalDistance());
-        } catch (FileFormatException e) {
-            fail("Should not throw exception.");
-        }
-    }
-
-    @Test
-    public void activityTotalDistanceCorrect3() {
-        try {
-            Parser testParser = new Parser(new File("src/test/java/seng202/group2/controllerTest/testData/blockrun.csv"));
-            double distance = 0.0;
-
-            distance += DataAnalyzer.calcDistance(30.245576, -97.823843, 30.246356, -97.823326);
-            distance += DataAnalyzer.calcDistance(30.246356, -97.823326, 30.246539, -97.821931);
-            distance += DataAnalyzer.calcDistance(30.246539, -97.821931, 30.247105, -97.821064);
-            distance += DataAnalyzer.calcDistance(30.247105, -97.821064, 30.247719, -97.820641);
-            distance += DataAnalyzer.calcDistance(30.247719, -97.820641, 30.248482, -97.820708);
-            distance += DataAnalyzer.calcDistance(30.248482, -97.820708, 30.24915, -97.820722);
-            ArrayList<Activity> test = testParser.getActivitiesRead();
-            assertEquals(distance, test.get(0).getTotalDistance());
-        } catch (FileFormatException e) {
-            fail("Should not throw exception.");
-        }
-    }
-
-    @Test
-    public void activityTotalDistanceCorrect4() {
-        try {
-            Parser testParser = new Parser(new File("src/test/java/seng202/group2/controllerTest/testData/blockrun.csv"));
-            double distance = 0.0;
-
-            distance += DataAnalyzer.calcDistance(30.245576, -97.823843, 30.246356, -97.823326);
-            distance += DataAnalyzer.calcDistance(30.246356, -97.823326, 30.246539, -97.821931);
-            distance += DataAnalyzer.calcDistance(30.246539, -97.821931, 30.247105, -97.821064);
-            distance += DataAnalyzer.calcDistance(30.247105, -97.821064, 30.247719, -97.820641);
-            distance += DataAnalyzer.calcDistance(30.247719, -97.820641, 30.248482, -97.820708);
-            distance += DataAnalyzer.calcDistance(30.248482, -97.820708, 30.24915, -97.820722);
-            ArrayList<Activity> test = testParser.getActivitiesRead();
-            assertEquals(distance, test.get(0).getTotalDistance());
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
@@ -145,7 +107,7 @@ public class ParserTest {
             ArrayList<Activity> test = testParser.getActivitiesRead();
             assertEquals(12, size(test));
 
-        } catch (FileFormatException e) {
+        } catch (Exception e) {
             fail("Should not throw exception.");
         }
     }
