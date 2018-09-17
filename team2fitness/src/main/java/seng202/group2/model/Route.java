ecb31883d0d56602cb7036013b57b80d86cebf8e
vsh33 <vsh33@uclive.ac.nz>
2018-09-17 22:05:18 +1200

    Map functionality implemented and working.

---
+++ team2fitness/src/main/java/seng202/group2/model/Route.java
@@ -0,0 +1,20 @@
+package seng202.group2.model;
+
+import java.util.ArrayList;
+
+public class Route {
+    private ArrayList<DataPoint> path = new ArrayList<DataPoint>();
+
+    public Route(ArrayList<DataPoint> points) {
+        path = points;
+    }
+
+    public String toJSONArray() {
+        StringBuilder stringBuild = new StringBuilder();
+        stringBuild.append("[");
+        path.forEach(point -> stringBuild.append(
+                String.format("{lat: %f, lng: %f}, ", point.getLatitude(), point.getLongitude())));
+        stringBuild.append("]");
+        return stringBuild.toString();
+    }
+}
