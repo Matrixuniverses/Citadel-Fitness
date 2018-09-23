f37e68c89a2830d7bcf3475801b9d3acb146c44c
vsh33 <vsh33@uclive.ac.nz>
2018-09-23 22:31:53 +1200

    More detailed error messages added for input validation.

--- team2fitness/src/main/java/seng202/group2/view/EditProfileController.java
+++ team2fitness/src/main/java/seng202/group2/view/EditProfileController.java
@@ -40,7 +40,19 @@ public class EditProfileController implements Initializable, UserData {
     private Button closeButton;
 
     @FXML
-    private Label errorLabel;
+    private Label nameErrorLabel;
+
+    @FXML
+    private Label heightErrorLabel;
+
+    @FXML
+    private Label ageErrorLabel;
+
+    @FXML
+    private Label weightErrorLabel;
+
+    @FXML
+    private Label confirmLabel;
 
     private DataManager dataManager;
 
@@ -48,6 +60,11 @@ public class EditProfileController implements Initializable, UserData {
     User currentUser;
 
     public void initialize(URL location, ResourceBundle resources) {
+        nameErrorLabel.setTextFill(Color.RED);
+        heightErrorLabel.setTextFill(Color.RED);
+        weightErrorLabel.setTextFill(Color.RED);
+        ageErrorLabel.setTextFill(Color.RED);
+        confirmLabel.setTextFill(Color.RED);
     }
 
 
@@ -72,47 +89,129 @@ public class EditProfileController implements Initializable, UserData {
         weightField.setText(Double.toString(currentUser.getWeight()));
     }
 
+
+
     /**
      * Updates the user's data with the fields provided. User information is changed and written to database.
      */
-    public void update() {
+/*    public void update() {
+        nameErrorLabel.setVisible(false);
+        ageErrorLabel.setVisible(false);
+        heightErrorLabel.setVisible(false);
+        weightErrorLabel.setVisible(false);
+        confirmLabel.setVisible(false);
 
         try {
-            errorLabel.setTextFill(Color.RED);
-
             String name = nameField.getText();
             int age = Integer.valueOf(ageField.getText());
             double height = Double.valueOf(heightField.getText());
             double weight = Double.valueOf(weightField.getText());
 
             if (name.length() > 25) {
-                errorLabel.setVisible(true);
-                errorLabel.setText("Name cannot exceed 25 characters.");
+                nameErrorLabel.setVisible(true);
+                nameErrorLabel.setText("Name cannot exceed 25 characters.");
             } else if (age < 0) {
-                errorLabel.setVisible(true);
-                errorLabel.setText("Age value cannot be negative.");
+                ageErrorLabel.setVisible(true);
+                ageErrorLabel.setText("Age value cannot be negative.");
             } else if (height <= 0 || weight <= 0) {
-                errorLabel.setVisible(true);
-                errorLabel.setText("Height/Weight values must be positive numbers.");
+                heightErrorLabel.setVisible(true);
+                heightErrorLabel.setText("Height/Weight values must be positive numbers.");
             } else {
                 currentUser.setAge(age);
                 currentUser.setHeight(height);
                 currentUser.setWeight(weight);
-
-                errorLabel.setTextFill(Color.BLACK);
+                confirmLabel.setVisible(true);
                 if (nameField.getText().length() == 0) {
-                    errorLabel.setVisible(true);
-                    errorLabel.setText("Age/Height/Weight values updated.");
+                    confirmLabel.setText("Age/Height/Weight values updated.");
                 } else {
                     currentUser.setName(nameField.getText());
-                    errorLabel.setVisible(true);
-                    errorLabel.setText("All values updated.");
+                    confirmLabel.setText("All values updated.");
                 }
             }
         } catch (NumberFormatException e) {
-            errorLabel.setVisible(true);
-            errorLabel.setText("Age/Height/Weight values must contain numbers.");
+            ageErrorLabel.setVisible(true);
+            ageErrorLabel.setText("Age must be a number.");
+            weightErrorLabel.setVisible(true);
+            weightErrorLabel.setText("Weight must be a number.");
+            heightErrorLabel.setVisible(true);
+            heightErrorLabel.setText("Height must be a number.");
         }
+    }*/
+
+    /**
+     * Updates the user's data with the fields provided. User information is changed and written to database.
+     */
+    public void update() {
+
+        nameErrorLabel.setText("");
+        ageErrorLabel.setText("");
+        heightErrorLabel.setText("");
+        weightErrorLabel.setText("");
+
+
+        boolean update = true;
+        int age = 0;
+        double height = 0.0;
+        double weight = 0.0;
+
+        String name = nameField.getText();
+        if (name.length() > 25) {
+            nameErrorLabel.setText("Name cannot exceed 25 characters");
+
+            update = false;
+        } else if (name.length() == 0) {
+            nameErrorLabel.setText("Name can't be empty.");
+            update = false;
+        }
+
+        try {
+            age = Integer.valueOf(ageField.getText());
+            if (age > 105 || age < 0) {
+                ageErrorLabel.setText("Age must be between 0 and 105");
+                update = false;
+            }
+        } catch (NumberFormatException e) {
+            ageErrorLabel.setText("Age must be an integer");
+            update = false;
+        }
+
+        try {
+            height = Double.valueOf(heightField.getText());
+            if (height > 270 || height < 0) {
+                heightErrorLabel.setText("Height must be between 0 and 270");
+                update = false;
+            }
+        } catch (NumberFormatException e) {
+            heightErrorLabel.setVisible(true);
+            heightErrorLabel.setText("Height must be a number");
+            update = false;
+        }
+
+        try {
+            weight = Double.valueOf(weightField.getText());
+            if (weight > 600 || weight < 0) {
+                weightErrorLabel.setText("Weight must be between 0 and 600");
+                update = false;
+            }
+        } catch (NumberFormatException e) {
+            weightErrorLabel.setVisible(true);
+            weightErrorLabel.setText("Weight must be an integer");
+            update = false;
+        }
+
+        if (update) {
+            currentUser.setName(name);
+            currentUser.setAge(age);
+            currentUser.setWeight(weight);
+            currentUser.setHeight(height);
+            confirmLabel.setTextFill(Color.GREEN);
+            confirmLabel.setText("User data updated.");
+        } else {
+            confirmLabel.setText("User data invalid. Profile update failed.");
+        }
+
+
+
     }
 
     public Button getCloseButton() {
