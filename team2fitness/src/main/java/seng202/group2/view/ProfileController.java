a0dde0ee84b38df56729284a3e754b01c306b0cd
osh17 <osh17@uclive.ac.nz>
2018-10-07 17:53:32 +1300

    Added target table on summary view.

--- team2fitness/src/main/java/seng202/group2/view/ProfileController.java
+++ team2fitness/src/main/java/seng202/group2/view/ProfileController.java
@@ -8,9 +8,11 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.Chart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.ProgressBarTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import seng202.group2.data.DataManager;
+import seng202.group2.model.Target;
 import seng202.group2.model.User;
 
 import java.net.URL;
@@ -38,28 +40,35 @@ public class ProfileController implements Initializable, UserData {
     }
 
     @FXML
-    Chart activityGraph;
+    private Chart activityGraph;
 
     @FXML
-    Label bmiLabel;
+    private Label bmiLabel;
 
     @FXML
-    Label weightLabel;
+    private Label weightLabel;
 
     @FXML
-    Label totalDistanceLabel;
+    private Label totalDistanceLabel;
 
     @FXML
-    Label targetsLabel;
+    private Label targetsLabel;
 
+    // Table
+    @FXML
+    private TableView<Target> targetTable;
 
     @FXML
-    Button editProfileButton;
+    private TableColumn nameColumn;
 
-    public Button getEditProfileButton() {
-        return editProfileButton;
-    }
+    @FXML
+    private TableColumn typeColumn;
 
+    @FXML
+    private TableColumn progressColumn;
+
+    @FXML
+    private TableColumn statusColumn;
 
     public void initialize(URL location, ResourceBundle resources) {
         dataManager.currentUserProperty().addListener(new ChangeListener<User>() {
@@ -68,9 +77,57 @@ public class ProfileController implements Initializable, UserData {
                 bmiLabel.textProperty().bind(Bindings.format("%.0f", dataManager.getCurrentUser().bmiProperty()));
                 weightLabel.textProperty().bind(Bindings.format("%.0f",dataManager.getCurrentUser().weightProperty()));
                 totalDistanceLabel.textProperty().bind(Bindings.format("%.1f",dataManager.getCurrentUser().totalDistanceProperty().divide(1000)));
+
             }
         });
 
+        targetTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        targetTable.setPlaceholder(new Label("No targets set."));
+
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Target, String>("name"));
+        typeColumn.setCellValueFactory(new PropertyValueFactory<Target, String>("formattedType"));
+        progressColumn.setCellValueFactory(new PropertyValueFactory<Target, Double>("progress"));
+        progressColumn.setCellFactory(ProgressBarTableCell.<Target> forTableColumn());
+        statusColumn.setCellValueFactory(new PropertyValueFactory<Target, String>("formattedStatus"));
+
+        dataManager.currentUserProperty().addListener(new ChangeListener<User>() {
+            @Override
+            public void changed(ObservableValue<? extends User> observable, User oldValue, User newValue) {
+                targetTable.setItems(DataManager.getDataManager().getTargetList());
+            }
+        });
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        for (Target target : dataManager.getCurrentUser().getTargetList()) {
+            System.out.println(target.getName());
+            switch(target.getType()) {
+                case "Total Distance (m)":
+                    dataManager.getCurrentUser().totalDistanceProperty().addListener(new ChangeListener<Number>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                            target.updateProgress((double) newValue);
+                        }
+                    });
+                case "Target Weight (kg)":
+                    dataManager.getCurrentUser().weightProperty().addListener(new ChangeListener<Number>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                            target.updateProgress((double) newValue);
+                            System.out.println(newValue);
+                        }
+                    });
+                case "Average Speed (m/s)":
+                    dataManager.getCurrentUser().avgSpeedProperty().addListener(new ChangeListener<Number>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                            target.updateProgress((double) newValue);
+                        }
+                    });
+            }
+        }
     }
 
         @Override
